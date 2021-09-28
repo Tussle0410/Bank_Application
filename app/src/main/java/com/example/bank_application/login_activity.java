3f9f1c2b9be33c17529bc4a0aff17c5c5893ca56
Tussle0410 <69793388+Tussle0410@users.noreply.github.com>
2021-09-28 23:54:29 +0900

    commit

---
+++ app/src/main/java/com/example/bank_application/login_activity.java
@@ -0,0 +1,13 @@
+package com.example.bank_application;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class login_activity extends AppCompatActivity {
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.login_page);
+
+    }
+}
