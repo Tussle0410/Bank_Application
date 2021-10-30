6c5deb299eb4155e178e389cc85e04af002d8807
Tussle0410 <69793388+Tussle0410@users.noreply.github.com>
2021-10-30 23:55:05 +0900

    commit

--- app/src/main/java/com/example/bank_application/remittance_amount_activity.java
+++ app/src/main/java/com/example/bank_application/remittance_amount_activity.java
@@ -1,6 +1,7 @@
 package com.example.bank_application;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.HandlerThread;
@@ -29,6 +30,7 @@ public class remittance_amount_activity extends AppCompatActivity {
     private TextView address,limit,money,receive_address;
     private EditText amount;
     private Button next_button,ten_thousand_plus,fifty_thousand_plus,hundred_thousand_plus;
+    private Button million_plus,all_plus;
     private String JsonString;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -50,12 +52,37 @@ public class remittance_amount_activity extends AppCompatActivity {
         money.setText(getIntent().getExtras().getString("Money"));
 
         amount = (EditText) findViewById(R.id.remittance_amount);
+        next_button = (Button) findViewById(R.id.remittance_amount_button);
         ten_thousand_plus = (Button) findViewById(R.id.remittance_tenThousand_plus);
+        fifty_thousand_plus = (Button) findViewById(R.id.remittance_fiftyThousand_plus);
+        hundred_thousand_plus = (Button) findViewById(R.id.remittance_hundredThousand_plus);
+        million_plus = (Button) findViewById(R.id.remittance_million_plus);
+        all_plus = (Button) findViewById(R.id.remittance_all_plus);
         ten_thousand_plus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 amountChange(10000);
         }});
+        fifty_thousand_plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {amountChange(50000);}});
+        hundred_thousand_plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {amountChange(100000); }});
+        million_plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {amountChange(1000000); }});
+        all_plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {amountChange(Integer.parseInt(money.getText().toString()));
+            }
+        });
+        next_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent next_intent = new Intent(getApplicationContext(),remittance_check_activity.class);
+            }
+        });
 
     }
     public void amountChange(int add_Money){
---
+++ app/src/main/java/com/example/bank_application/remittance_check_activity.java
@@ -0,0 +1,14 @@
+package com.example.bank_application;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class remittance_check_activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.remittance_check_page);
+    }
+}
