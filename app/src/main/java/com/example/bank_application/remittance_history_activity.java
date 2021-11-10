52ca3a94fe2801977329300c20b359c5580a9212
Tussle0410 <69793388+Tussle0410@users.noreply.github.com>
2021-11-10 23:55:51 +0900

    commit

--- app/src/main/java/com/example/bank_application/remittance_history_activity.java
+++ app/src/main/java/com/example/bank_application/remittance_history_activity.java
@@ -1,4 +1,30 @@
 package com.example.bank_application;
 
-public class remittance_history_activity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class remittance_history_activity extends AppCompatActivity {
+    private ImageButton back_button;
+    private Button deposit,saving,funding,loan;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.remittance_history_page);
+        back_button = (ImageButton) findViewById(R.id.remittance_history_back_button);
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { finish(); }});
+        deposit = (Button)findViewById(R.id.remittance_history_deposit_button);
+        saving = (Button)findViewById(R.id.remittance_history_savings_button);
+        funding = (Button)findViewById(R.id.remittance_history_funding_button);
+        loan = (Button)findViewById(R.id.remittance_history_loan_button);
+
+
+    }
 }
