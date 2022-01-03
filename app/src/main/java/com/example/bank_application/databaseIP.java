c94538ca7113ed36995673e80c33d425cd594110
Tussle0410 <69793388+Tussle0410@users.noreply.github.com>
2022-01-04 02:07:39 +0900

    commit

--- app/src/main/java/com/example/bank_application/databaseIP.java
+++ app/src/main/java/com/example/bank_application/databaseIP.java
@@ -3,7 +3,7 @@ package com.example.bank_application;
 import android.app.Application;
 
 public class databaseIP extends Application {       //Application 이용한 전역변수 선언
-    private String IP_Address = "192.168.0.3";      //Database 서버 주소
+    private String IP_Address = "172.30.1.15";      //Database 서버 주소
 
     public String getIP_Address() {
         return IP_Address;
